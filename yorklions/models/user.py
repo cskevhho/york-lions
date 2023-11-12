693a94baa1d626da5fe0bd86e767ef91a79cca9b
Kevin <kevhho@my.yorku.ca>
2023-11-12 18:15:14 -0500

    small fixes

--- yorklions/models/user.py
+++ yorklions/models/user.py
@@ -2,7 +2,6 @@ from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 from ..extensions import db
 
-
 class User(db.Model):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
--- yorklions/routes/user/create.py
+++ yorklions/routes/user/create.py
@@ -6,4 +6,4 @@ def create_user(username, email, password):
     new_user = User(username=username, email=email, password=password)
     db.session.add(new_user)
     db.session.commit()
-    return {"message": f"User '{new_user.name}' created successfully"}, 200
+    return {"message": f"User '{new_user.username}' created successfully"}, 200
