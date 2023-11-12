366b2f11e0da5c2f1cea3b28571cf57a187c8620
Kevin <kevhho@my.yorku.ca>
2023-11-12 10:51:33 -0500

    Corrected read all function

--- yorklions/routes/user/read.py
+++ yorklions/routes/user/read.py
@@ -6,4 +6,6 @@ def read_user():
     users = db.session.query(User).all()
     if not users:
         return {"message": "No users found"}, 400
-    return {"users": [user.to_dict() for user in users]}, 200
+    
+    user_data = [{"id": user.id, "name": user.name} for user in users]  # note this is will return that json format response thing
+    return {"users": user_data}, 200
