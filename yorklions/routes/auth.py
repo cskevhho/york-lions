13c30837e9d443f466535870c931ec295ab8cbdf
Kevin <kevhho@my.yorku.ca>
2023-11-13 13:25:59 -0500

    Small fixes to last commit

--- yorklions/routes/auth.py
+++ yorklions/routes/auth.py
@@ -4,14 +4,13 @@ from ..forms import RegistrationForm, LoginForm
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/register")
+@auth.route("/register", methods=["GET","POST"])
 def register():
     form = RegistrationForm()
     return render_template("register.html", title="Register", form=form)
 
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    # return render_template("login.html", title="Login", form=form)
-    return render_template("login.html")
+    return render_template("login.html", title="Login", form=form)
