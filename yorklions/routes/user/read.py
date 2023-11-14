a97fe61b25801efbcae57c82a3b5c17c821116b0
Andrew Hocking <github@hocking.tech>
2023-11-14 17:59:31 -0500

    Extended read_user functionality
    Modified the read_user method to allow for searching by specific attributes as well as displaying all users

--- yorklions/routes/user/read.py
+++ yorklions/routes/user/read.py
@@ -1,9 +1,21 @@
 from ...models.user import User
 from ...extensions import db
+from flask import request
 
 
 def read_user():
-    users = db.session.query(User).all()
+    search_type = request.form['search_type']
+    search_text = request.form['search_field']
+
+    if search_type == "user_id":
+        users = db.session.query(User).filter_by(id=search_text).all()
+    elif search_type == "username":
+        users = db.session.query(User).filter_by(username=search_text).all()
+    elif search_type == "email":
+        users = db.session.query(User).filter_by(email=search_text).all()
+    else:
+        users = db.session.query(User).all()
+
     if not users:
         return {"message": "No users found"}, 400
 
