3a0e3bdea4b3c46899f0026730de8a8e1210190c
Mustafa Rizvi <mustafa_rizvi@hotmail.com>
2023-11-17 16:20:45 -0500

    LOAN CALCULATOR DONE

--- yorklions/__init__.py
+++ yorklions/__init__.py
@@ -4,6 +4,7 @@ from .routes.user.controller import user_ctrl
 from .routes.vehicle.controller import vehicle_ctrl
 from .routes.auth import auth
 from .routes.main import main
+from .routes.loancalc import loancalc
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
@@ -16,6 +17,7 @@ bcrypt.init_app(app)
 login_manager.init_app(app)
 app.register_blueprint(main)
 app.register_blueprint(auth)
+app.register_blueprint(loancalc)
 app.register_blueprint(user_ctrl, url_prefix="/api/user")
 app.register_blueprint(vehicle_ctrl, url_prefix="/api/vehicle")
 
---
+++ yorklions/routes/loancalc.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template, request
+
+loancalc = Blueprint("loancalc", __name__)
+
+@loancalc.route('/loan-calculator/', methods=['GET', 'POST'])
+def loan_calculator():
+    if request.method == 'POST':
+        vehicle_price = float(request.form['vehiclePrice'])
+        down_payment = float(request.form['downPayment'])
+        loan_interest_rate = float(request.form['loanInterestRate'])
+        loan_duration = int(request.form['loanDuration'])
+
+        remaining_loan_amount = vehicle_price - down_payment
+        monthly_interest = (loan_interest_rate / 100) / 12
+        monthlypayment = remaining_loan_amount * (monthly_interest/(1-(1+monthly_interest) ** - loan_duration))
+        monthlypayment = round(monthlypayment, 2)
+        return render_template('loancalculator.html', monthly_payment=monthlypayment)
+
+    return render_template('loancalculator.html')
