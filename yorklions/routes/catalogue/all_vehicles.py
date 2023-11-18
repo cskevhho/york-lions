86c59835c9d40fe5d6640cba0ad5174d78fe5f66
Andrew Hocking <github@hocking.tech>
2023-11-18 18:45:21 -0500

    Implemented "Shop Now" page
    Resolves #32

---
+++ yorklions/routes/catalogue/all_vehicles.py
@@ -0,0 +1,16 @@
+from ...models.vehicle import Vehicle
+from ...extensions import db
+from ..vehicle.utils import vehicle_json
+
+def get_all_vehicles(limit=None):
+    if limit:
+        vehicles = db.session.query(Vehicle).filter(Vehicle.is_for_sale == True).limit(limit).all()
+    else:
+        vehicles = db.session.query(Vehicle).filter(Vehicle.is_for_sale == True).all()
+    # TODO: .paginate(page=page, per_page=per_page, error_out=error_out, max_per_page=max_per_page)
+
+    if not vehicles:
+        return {"message": "No vehicles found"}, 400
+
+    vehicle_data = vehicle_json(vehicles) # note this is will return that json format response thing
+    return vehicle_data, 200
