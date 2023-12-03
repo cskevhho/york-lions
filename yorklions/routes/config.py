e6987869c9b0b37c7720bbaf66d31c38392a61b1
Gary Hu <garyhu2015@gmail.com>
2023-12-03 10:06:54 -0500

    Delete yorklions/routes/config.py

--- yorklions/routes/config.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-import hashlib
-
-def configure_app(app):
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
-    h = hashlib.new('sha256')
-    h.update(b"1234")
-    app.config["SECRET_KEY"] = h.digest()
-    return app
