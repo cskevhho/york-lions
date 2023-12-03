1d5a6c0fd0273bb7aeda98642dff8952e9aa5f78
Gary Hu <garyhu2015@gmail.com>
2023-12-03 09:58:41 -0500

    Add files via upload

---
+++ yorklions/routes/vehicle/config.py
@@ -0,0 +1,8 @@
+from flask import Flask
+import hashlib
+
+def configure_app(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+    h = hashlib.new('sha256')
+    h.update(b"1234")
+    app.config["SECRET_KEY"] = h.digest()
