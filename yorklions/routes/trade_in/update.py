9f4ddb3f5d387e19bde4f04931bd5cda42c06890
Andrew Hocking <github@hocking.tech>
2023-12-04 11:50:54 -0500

    Ignore blank fields in admin trade-in update
    Blank fields will be ignored in admin trade-in update screen

--- yorklions/routes/trade_in/update.py
+++ yorklions/routes/trade_in/update.py
@@ -7,15 +7,35 @@ def update_trade_in(id, updated_data):
     if not trade_in:
         return {"message": f"Trade-In with ID {id} not found"}, 404
 
-
     trade_in.id = updated_data.get("id", trade_in.id)
-    trade_in.user_id = updated_data.get("user_id", trade_in.user_id)
-    trade_in.vehicle_id = updated_data.get("vehicle_id", trade_in.vehicle_id)
-    trade_in.status = updated_data.get("status", trade_in.status)
-    trade_in.quote = updated_data.get("quote", trade_in.quote)
-    trade_in.accidents = updated_data.get("accidents", trade_in.accidents)
-    trade_in.details = updated_data.get("details", trade_in.details)
-    trade_in.date_updated = updated_data.get("date_updated", trade_in.date_updated)
+    
+    new_value = updated_data.get("user_id")
+    if new_value and new_value != "":
+        trade_in.user_id = updated_data.get("user_id", trade_in.user_id)
+
+    new_value = updated_data.get("vehicle_id")
+    if new_value and new_value != "":
+        trade_in.vehicle_id = updated_data.get("vehicle_id", trade_in.vehicle_id)
+
+    new_value = updated_data.get("status")
+    if new_value and new_value != "":
+        trade_in.status = updated_data.get("status", trade_in.status)
+
+    new_value = updated_data.get("quote")
+    if new_value and new_value != "":
+        trade_in.quote = updated_data.get("quote", trade_in.quote)
+
+    new_value = updated_data.get("accidents")
+    if new_value and new_value != "":
+        trade_in.accidents = updated_data.get("accidents", trade_in.accidents)
+
+    new_value = updated_data.get("details")
+    if new_value and new_value != "":
+        trade_in.details = updated_data.get("details", trade_in.details)
+
+    new_value = updated_data.get("date_updated")
+    if new_value and new_value != "":
+        trade_in.date_updated = updated_data.get("date_updated", trade_in.date_updated)
 
     db.session.commit()
     return {"message": f"Trade-In '{trade_in.id}' updated successfully"}, 200
