4d22cfa5f700cd0bc1e1787ea136290d69019b7d
Gary Hu <garyhu2015@gmail.com>
2023-12-06 02:10:56 -0500

    Update reports.py

--- yorklions/routes/reports.py
+++ yorklions/routes/reports.py
@@ -1,11 +1,14 @@
 from ...models.visitevent import VisitEvent
 from ...models.po import PurchaseOrder
 from ...extensions import db
-from flask import render_template, session
-from flask_login import current_user
+from flask import Flask, request, render_template, session
 
-def generate_sales_report_descending():
-    sales = db.session.query(PurchaseOrder).order_by(PurchaseOrder.date.desc()).all()
+@app.route('/get-sales-report', methods=['GET', 'POST'])
+def generate_sales_report():
+    if descending:
+        sales = db.session.query(PurchaseOrder).order_by(PurchaseOrder.date.desc()).all()
+    else:
+        sales = db.session.query(PurchaseOrder).order_by(PurchaseOrder.date).all()
     salesReport = [
         {
             "id": po.id,
@@ -20,22 +23,7 @@ def generate_sales_report_descending():
     ]
     return render_template('admin.html', salesReport_des=salesReport_des), 200
 
-def generate_sales_report_ascending():
-    sales = db.session.query(PurchaseOrder).order_by(PurchaseOrder.date).all()
-    salesReport = [
-        {
-            "id": po.id,
-            "fnmae": po.fname,
-            "lname": po.lname,
-            "status": po.ststus,
-            "address id": po.address_id,
-            "purchase orders": po.purchase_orders,
-            "date": po.date 
-        }
-        for po in sales
-    ]
-    return render_template('admin.html', salesReport_asc=salesReport_asc), 200
-
+@app.route('/get-usage-report', methods=['GET', 'POST'])
 def generate_app_usage():
     events = db.session.query(VisitEvent).order_by(VisitEvent.date.desc()).all()
     eventReport = [
